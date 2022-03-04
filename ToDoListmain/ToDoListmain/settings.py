a2ff51cfddefe3abd3bdf7a5420c8b9933ee6b54
HaroonRasikh <89982158+HaroonRasikh@users.noreply.github.com>
2022-03-05 00:02:20 +0300

    Update settings.py

--- ToDoListmain/ToDoListmain/settings.py
+++ ToDoListmain/ToDoListmain/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ToDoListmain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-       # 'DIRS': ['Users/HP/Desktop/new/ToDoListmain/todo/templates'],
+       # 'DIRS': [BASE_DIR/'templates'],
        'DIRS':[BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
